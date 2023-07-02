28153c915f178a8cfb55ddb088eaa40545209ac9
Mahdi <zafarmand.mahdi@gmail.com>
2023-07-02 14:50:09 -0400

    fix missing function body

--- vmcheck/cpuidlow_amd64.s
+++ vmcheck/cpuidlow_amd64.s
@@ -1,6 +1,6 @@
 #include "textflag.h"
 
-// func cpuid_low(arg1 uint32, arg2 uint32) (eax uint32, ebx uint32, ecx uint32, edx uint32)
+// func cpuid_low(arg1, arg2 uint32) (eax, ebx, ecx, edx uint32)
 TEXT ·cpuid_low(SB), NOSPLIT, $0-24
 	MOVL arg1+0(FP), AX
 	MOVL arg2+4(FP), CX
--- vmcheck/vmcheck.go
+++ vmcheck/vmcheck.go
@@ -1,15 +1,13 @@
-// Package vmcheck contains the code to read cpuid information and prints them out
+// Package main contains the code to read cpuid information and prints them out
 
-package vmcheck
+package main
 
 import (
 	"encoding/binary"
 	"fmt"
 )
 
-// Get the CPU ID low level leaf values.
-func cpuid_low(arg1, arg2 uint32) (eax, ebx, ecx, edx uint32)
-
+func cpuid_low(arg1, arg2 uint32) (eax, ebx, ecx, edx uint32) // implemented in cpuidlow_amd64.s
 func main() {
 	var HV uint32
 	HV = uint32(1 << 31)
